c9ef15087a04e27bbdec32f9400bbf02aa44368a
IgorBolotnikov <igorbolotnikov1993@gmail.com>
2021-03-19 08:28:26 +0200

    Populate dictionary

---
+++ src/Calculation/constants.ts
@@ -0,0 +1,33 @@
+export const NUMBER_TO_WORD_MAP = {
+  '1': 'eins',
+  '2': 'zwei',
+  '3': 'drei',
+  '4': 'vier',
+  '5': 'fünf',
+  '6': 'sechs',
+  '7': 'sieben',
+  '8': 'acht',
+  '9': 'neun',
+
+  '10': 'zehn',
+  '11': 'elf',
+  '12': 'zwölf',
+  '13': 'dreizehn',
+  '14': 'vierzehn',
+  '15': 'fünfzehn',
+  '16': 'sechszehn',
+  '17': 'siebzehn',
+  '18': 'achtzehn',
+  '19': 'neunzehn',
+
+  '20': 'zwanzig',
+  '30': 'dreißig',
+  '40': 'vierzig',
+  '50': 'fünfzig',
+  '60': 'sechszig',
+  '70': 'siebzig',
+  '80': 'achtzig',
+  '90': 'neunzig',
+
+  '100': 'einhundert',
+};
